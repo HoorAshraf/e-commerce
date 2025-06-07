ccb4b57bf0a0797e26eba0aa5e1c3e9a5a25db71
HoorAshraf <horah0664@gmail.com>
2025-06-07 15:44:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Fresh-Cart/',  // Set the base to the GitHub Pages path
+   base: '/e-commerce/',
 });

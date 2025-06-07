15ef903efbd67db9315f1083f72c3580fa58ab81
HoorAshraf <horah0664@gmail.com>
2025-06-07 15:52:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/e-commerce/', 
   plugins: [react()],
-   base: '/e-commerce/',
-});
+})
